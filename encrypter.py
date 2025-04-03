a60315f5a613eece98e0ee09d84d2c57e5297f3c
Ramon Siqueira <152916035+Rmns28@users.noreply.github.com>
2025-04-02 22:52:42 -0300

    Create encrypter.py
    Criando arquivo de criptografia usando o terminal + nano do kali usando do linguagem Pyton

---
+++ encrypter.py
@@ -0,0 +1,24 @@
+import os
+import pyaes
+
+## abrir o arquivo criptografado
+file_name = "teste.txt"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+## remover arquivo
+os.remove(file_name)
+
+## chave de criptografia
+key = b"responsabilidade"
+aes = pyaes.AESModeOfOperationCTR(key)
+
+## criptografar o arquivo
+crypto_data = aes.encrypt(file_data)
+
+## salvar o arquivo criptografado
+new_file = file_name + ".arquivocriptografado"
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
+new_file.close()

462be278221a7e6fd3e619f6147ef82bea2d12ac
Ramon Siqueira <152916035+Rmns28@users.noreply.github.com>
2025-04-02 23:13:32 -0300

    create decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -1,7 +1,7 @@
 import os
 import pyaes
 
-## abrir o arquivo criptografado
+## abrir arquivo criptografado
 file_name = "exemplo.txt.arquivocriptografado"
 file = open(file_name, "rb")
 file_data = file.read()
@@ -12,10 +12,10 @@ key = b"responsabilidade"
 aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
+## remover arquivo criptografado
 os.remove(file_name)
 
-## criar o arquivo descriptografado
+## criar arquivo descriptografado
 new_file = "exemplo.txt"
 new_file = open(f'{new_file}', "wb")
 new_file.write(decrypt_data)

